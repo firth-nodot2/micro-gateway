ce38e9fe362edf99b362720b3c548db6d4469a33
firth-nodot2 <firthnodot2@gmail.com>
2025-07-20 15:23:22 +0800

    First Commit

---
+++ src/main/java/org/rhydo/microgateway/MicroGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.rhydo.microgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rhydo/microgateway/MicroGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rhydo.microgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
